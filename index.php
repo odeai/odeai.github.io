21e46b3a1173c6c6e8c24a5d046e9795ae62edd6
陳文風 <chin@cyberstep.com>
2020-03-14 01:10:32 +0900

    app

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title>melody</title>
+	</head>
+	<body>
+		<h1>Melody</h1>
+		<p>An new matching app!</p>
+		<img src="/icon.jpg" alt="">
+	</body>
+</html>
